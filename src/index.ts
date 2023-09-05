13ba1f7eccaf30fe7091a87d22eb4c33001dd213
karinadarc <darcsilva.kd@gmail.com>
2023-09-05 20:53:21 -0300

    exercício typescript II

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,20 @@
-import { users, produtos } from "./database"
+import { users, produtos, createUser, getAllUsers, getAllProducts, createProduct, searchUserByName, searchProductsByName } from "./database"
+import { TProduct, TUser } from "./types";
 
-console.table(users)
-console.table(produtos)
+//---------------------------------------------------------------------------------------------------
+//exercício 1
+// console.table(users)
+// console.table(produtos)
 
 
+
+//variavel         //função
+const allUsers = getAllUsers();
+console.log(allUsers);
+
+const allProduct = getAllProducts();
+console.log(allProduct);
+
+
+console.table(searchUserByName(users, 'barbie'))
+console.table(searchProductsByName(produtos, 'tv Samsung'))
